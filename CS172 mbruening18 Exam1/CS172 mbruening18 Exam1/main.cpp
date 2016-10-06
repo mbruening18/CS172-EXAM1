db96bb088ca444fd51aec31069c60c94474d22dd
Megan Bruening <mbruening18@my.whitworth.edu>
2016-10-06 14:22:03 -0700

    Start Exam

---
+++ CS172 mbruening18 Exam1/CS172 mbruening18 Exam1/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  CS172 mbruening18 Exam1
+//
+//  Created by Megan Bruening on 10/6/16.
+//  Copyright © 2016 Megan Bruening. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+    
+}
